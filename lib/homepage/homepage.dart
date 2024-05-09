c1840c62c7aee8c513e03962843fba5cafca8b5a
Fahmi Roslee <amiroslee20@gmail.com>
2024-05-09 17:43:46 +0800

    homepage

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:printex_business_app/components.dart';
 
 class Homepage extends StatefulWidget {
@@ -14,23 +19,102 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
         appBar: PrinTEXComponents()
             .appBar('PrinTEX Registered', context, null, showInfo: true),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.black,
+          child: const Icon(Icons.add, color: Colors.white),
+        ),
         body: Center(
-          child: Container(
-            height: MediaQuery.of(context).size.height * 0.5,
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: Column(
+            child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Image.asset('assets/images/printer-amico.png'),
-                const SizedBox(height: 10),
-                const Text(
-                  "No registered PrinTEX yet",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                Expanded(
+                  child: Container(
+                    height: 100, // Height of the first row
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(
+                          248, 238, 238, 238), // Background color
+                      borderRadius:
+                          BorderRadius.circular(10), // Rounded corners
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5), // Shadow color
+                          spreadRadius: 5, // Spread radius
+                          blurRadius: 7, // Blur radius
+                          offset: const Offset(0, 1), // Offset
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(color: Colors.grey),
+                            ),
+                            child:
+                                Image.asset('assets/images/printer-amico.png'),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.all(5.0),
+                                  child: Text(
+                                    "PrinTEX Perpustakaan ", //text AKAN OVERFLOWWWWWWW
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.all(5.0),
+                                  child: Text(
+                                    "Skudai, Johor", //text AKAN OVERFLOWWWWWWW
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(99, 99, 99, 1)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.all(5.0),
+                                  child: Container(
+                                    height: 25,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      border: Border.all(color: Colors.grey),
+                                    ),
+                                    child: Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 10),
+                                        child: Text('Active')),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                const SizedBox(height: 10),
-                const Text("Register PrinTEX now"),
               ],
             ),
-          ),
-        ));
+          ]),
+        )));
   }
 }
---
+++ lib/homepage/homepage2.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:printex_business_app/components.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PrinTEXComponents()
+            .appBar('PrinTEX Registered', context, null, showInfo: true),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.black,
+          child: const Icon(Icons.add, color: Colors.white),
+        ),
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Column(
+              children: [
+                Image.asset('assets/images/printer-amico.png'),
+                const SizedBox(height: 10),
+                const Text(
+                  "No registered PrinTEX yet",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                const SizedBox(height: 10),
+                const Text("Register PrinTEX now"),
+              ],
+            ),
+          ),
+        ));
+  }
+}
