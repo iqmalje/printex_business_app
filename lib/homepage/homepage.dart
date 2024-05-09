b47473991037105e30681c2e685a153cfecf4915
Fahmi Roslee <amiroslee20@gmail.com>
2024-05-09 14:57:07 +0800

    empty PrinTEX homepage

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -12,9 +12,25 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PrinTEXComponents()
-          .appBar('PrinTEX Registered', context, null, showInfo: true),
-      body: Center(child: Text('PrinTEX Registered Page')),
-    );
+        appBar: PrinTEXComponents()
+            .appBar('PrinTEX Registered', context, null, showInfo: true),
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Column(
+              children: [
+                Image.asset('assets/images/printer-amico.png'),
+                const SizedBox(height: 10),
+                const Text(
+                  "No registered PrinTEX yet",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                const SizedBox(height: 10),
+                const Text("Register PrinTEX now"),
+              ],
+            ),
+          ),
+        ));
   }
 }
